25e9c8fa9b5fe81ea8039a4c28a84b46da8e0fbd
Matt <matthewho.2021@scis.smu.edu.sg>
2024-02-12 15:33:17 +0800

    Integrated PIR

--- src/main.py
+++ src/main.py
@@ -2,14 +2,26 @@ import time
 import busio
 import board
 import adafruit_amg88xx
+import RPi.GPIO as GPIO
 
+# Set up thermal AMG8833
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 
+# Set up PIR
+GPIO.setmode(GPIO.BCM)
+PIR_PIN = 17
+
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+print('Startup, give it 3 seconds')
+time.sleep(3)
+print('READY')
+
 while True:
-    for row in amg.pixels:
-        # Pad to 1 decimal place
-        print(["{0:.1f}".format(temp) for temp in row])
-        print("")
-    print("\n")
-    time.sleep(4)
+    if not GPIO.input(PIR_PIN):
+        for row in amg.pixels:
+            print(["{0:.3f}".format(temp) for temp in row])
+            print("")
+        print("\n")
+        time.sleep(4)
