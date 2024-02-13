874e67fbe8aa9327919b253a037235a68fd66616
Matt <matthewho.2021@scis.smu.edu.sg>
2024-02-13 14:41:20 +0800

    Added acc

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import busio
 import board
 import adafruit_amg88xx
 import RPi.GPIO as GPIO
+import mpu6050
+import json
 
 # Frequency
 FREQ = 2
@@ -10,6 +12,21 @@ FREQ = 2
 # Set up thermal AMG8833
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
+
+mpu6050 = mpu6050.mpu6050(0x68)
+
+def read_acc_temp():
+    gyroscope_data = mpu6050.get_gyro_data()
+    temperature = mpu6050.get_temp()
+
+    return gyroscope_data, temperature
+
+def Record():
+    def __init__(self, therm, gyr, ambient_temp, has_motion):
+        self.therm = therm
+        self.gyr = gyr
+        self.ambient_temp = ambient_temp
+        self.has_motion = has_motion
 
 # Set up PIR
 GPIO.setmode(GPIO.BCM)
@@ -22,11 +39,15 @@ time.sleep(3)
 print('READY')
 
 while True:
-    if not GPIO.input(PIR_PIN):
-        for row in amg.pixels:
-            print(["{0:.3f}".format(temp) for temp in row])
-        print("\n")
-    else:
-        print("Not doing, presence detected")
+    # if not GPIO.input(PIR_PIN):
+    #     for row in amg.pixels:
+    #         print(["{0:.3f}".format(temp) for temp in row])
+    #     print("\n")
+    # else:
+    #     print("Not doing, presence detected")
+    gyroscope, ambient = read_acc_temp()
+    rec = Record(amg.pixels, gyroscope, ambient, GPIO.input(PIR_PIN))
+    
+    print(json.dumps(rec))
     
     time.sleep(FREQ)
